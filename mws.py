98be136e6b114895759426b79ac1a53458e24ac5
ZombieFly <60338092+ZombieFly@users.noreply.github.com>
2020-07-04 20:24:46 +0800

    Update mws.py
    1、优化代码，使之更为简洁、便于阅读
    2、变量置前，方便后期自定义修改
    3、支持参数搜索，!前的为参数，目前仅支持gp参数，用以在gamepedia原站搜索

--- mws.py
+++ mws.py
@@ -17,29 +17,87 @@ from urllib.parse import quote, unquote, urlencode
 
 
 
-@on_command('wiki',aliases=('维基'),only_to_me=False)
-async def findv(session: CommandSession):
-    msg=session.current_arg_text.strip()
-    final_url = 'https://wiki.biligame.com/mc/'+msg	#在此处可更改为mc wiki官方源
+bili_url = 'https://wiki.biligame.com/mc/'
+bili_null_head = 'lang="zh-Hans-CN">\n<p>'
+bili_null_tail = '<a href='
+bili_null_msg = ['本页面目前没有内容。您可以在其他页面中']
+
+gpd_url = 'https://minecraft-zh.gamepedia.com/'
+gpd_null_head = 'action=edit">'
+gpd_null_tail = '</a>，<b>请注意</b>：'
+gpd_null_msg = ['创建本页面']
+gpd_output_first = '此模式下需要数十秒的时间请求数据，请耐心等待'
+
+null_msg = '呼喵？没有找到该条目呢'
+msg_head = '搜索结果出来了喵：'
+summary_head = '</div>\n<p><b>'
+summary_head2 = '</table>\n<p><b>'
+summary_tail = '\n</p>\n'
+
+
+#bili镜像
+
+def bili(msg):
+    final_url = bili_url+msg
     final_res = requests.get(final_url)
     final_res.encoding = chardet.detect(final_res.content)['encoding']
     final_res = final_res.text
-    pattern = re.compile('lang="zh-Hans-CN">\n<p>(.*?)<a href=')
+
+    pattern = re.compile(bili_null_head + '(.*?)' + bili_null_tail)
     data = pattern.findall(final_res)
-    if data==['本页面目前没有内容。您可以在其他页面中']:
-        await session.send('没有找到条目"'+msg+'"，但你可以尝试查看相关搜索结果：\n'+'https://searchwiki.biligame.com/mc/index.php?search='+quote(msg))
+
+    #判断是否有该页面
+    if data==bili_null_msg:
+        print(null_msg)
     else:
-        pattern2 = re.compile('<p><b>(.*?)\n</p>\n<div')
-        tell = pattern2.findall(final_res)
+        #获取简介
+        bl_summary = re.sub(r"</?(.+?)>", "", ''.join(re.compile(summary_head + '(.*?)' + summary_tail).findall(final_res)))
+        if bl_summary == '':
+            bl_summary = re.sub(r"</?(.+?)>", "", ''.join(re.compile(summary_head2 + '(.*?)' + summary_tail).findall(final_res)))
+        #获取url
+        bltail_url = re.compile('/mc/(.*?)">阅读</a></li><li').findall(final_res)  
+        #拼接最后消息
+        return msg_head + bili_url + ''.join(bltail_url) + '\n' + bl_summary
 
-        pattern3 = re.compile('/mc(.*?)">阅读</a></li><li')
-        tell1 = pattern3.findall(final_res)
 
-        if tell==[]:
-            pattern2 = re.compile('<p><b>(.*?)\n</p>')
-            tell = pattern2.findall(final_res)
-        tell = ''.join(tell)
-        tell = re.sub(r"</?(.+?)>", "", tell)
 
-        output='有关"'+msg+'"的搜索结果出来了喵：'+'https://wiki.biligame.com/mc'+''.join(tell1)+'\n'+tell
+#源站点
+
+def gpd(msg):
+    final_url = gpd_url + msg
+    final_res = requests.get(final_url)
+    final_res.encoding = chardet.detect(final_res.content)['encoding']
+    final_res = final_res.text
+    
+    pattern = re.compile(gpd_null_head+'(.*?)'+gpd_null_tail)
+    data = pattern.findall(final_res)
+    
+    if data==gpd_null_msg:
+        print(null_msg)
+    else:
+
+        gpd_summary = re.sub(r"</?(.+?)>", "", ''.join(re.compile(summary_head + '(.*?)' + summary_tail).findall(final_res)))
+        if gpd_summary == []:
+            gpd_summary = re.sub(r"</?(.+?)>", "", ''.join(re.compile(summary_head2 + '(.*?)' + summary_tail).findall(final_res)))
+        
+        gpd_rurl = re.compile('<meta property="og:url" content="(.*?)"/>').findall(final_res)  
+        
+        return msg_head + ''.join(gpd_rurl) + '\n' + gpd_summary
+
+#获取参数
+def process_pt(msg):
+    pattern = re.compile('(.*?)!')
+    parameter = pattern.findall(msg)
+    if parameter != [] and parameter[0]=='':
+        parameter = parameter[1:]
+    return parameter
+
+@on_command('wiki',aliases=('维基'),only_to_me=False)
+async def findv(session: CommandSession):
+    msg=session.current_arg_text.strip()
+    if 'gp' in process_pt(msg):
         await session.send(output)
+        output='[gp]'+gpd(re.sub(r"(.+?)!", "", msg))
+    else:
+        output=bili(msg)
+    await session.send(output)
